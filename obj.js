34454d89eecd3414d06d66db76cfaae5ecfc78ff
Pozsa Marcell (SZF_E_2022_2024) <pozsa.marcell@diak.szamalk-szalezi.hu>
2023-04-15 12:48:36 +0200

    alap ötletek hozzáadása

---
+++ obj.js
@@ -0,0 +1,23 @@
+const Tutorial = [
+    
+    {},
+    {},
+    {},
+    {}
+];
+/*tutorial ötletek*/
+/*
+1.cooking chicke with slaps
+2.ez a vidi https://www.youtube.com/watch?v=Wa_WWGQwBhU
+3.how to cook a minute rice in 58 seconds
+4.how to imagine anything
+5.how to order food?
+6.ez a vidi? https://www.youtube.com/watch?v=zGAUwmWmZiU
+7.https://top-10-list.org/2013/02/02/top-10-most-useless-tutorials/
+8.
+
+
+kosár
+admin felület
+képek hozzáadsa alap leírssal,tutorial tipusok kiszerzeés
+*/
